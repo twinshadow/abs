156bc63ab59655ffbb01dd6a8b9be40945d839f0
Anthony Cornehl <accornehl@gmail.com>
2013-11-03 18:41:45 -0800

    cksum_update.py: put dummy cksum in before generating the real values

--- .scripts/cksum_update.py
+++ .scripts/cksum_update.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python3
 
-import os
-cksums = os.popen("makepkg -g").read().splitlines()
-fd = open("PKGBUILD")
-pkgbuild = fd.read().splitlines()
-fd.close()
+from subprocess import Popen, PIPE
+from sys import exit
 
-start = pkgbuild.index("# cksum start")
-end = pkgbuild.index("# cksum end")
-if start and end:
-    foo = pkgbuild[:start + 1]
-    foo += cksums
-    foo += pkgbuild[end:]
-    fd = open("PKGBUILD", "w")
-    fd.write('\n'.join(foo))
-    fd.close()
+blank_cksums = [
+    "md5sums=(0)\n",
+    "sha512sums=(0)\n",
+]
+
+with open("PKGBUILD") as fd:
+    pkgbuild = fd.readlines()
+
+top = pkgbuild[:pkgbuild.index("# cksum start\n") + 1]
+bot = pkgbuild[pkgbuild.index("# cksum end\n"):]
+
+with open("PKGBUILD", 'w') as fd:
+    fd.writelines(top + blank_cksums + bot)
+
+proc = Popen(("makepkg", "-g"), stdout=PIPE)
+cksums = proc.communicate()[0].decode().splitlines(True)
+if proc.returncode != 0:
+    exit(proc.returncode)
+
+with open("PKGBUILD", 'w') as fd:
+    fd.writelines(top + cksums + bot)
