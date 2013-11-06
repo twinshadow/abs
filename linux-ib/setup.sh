60a3941d99c2b80f10dea96a98b67862b0a1b469
Anthony Cornehl <accornehl@gmail.com>
2013-11-06 08:11:37 -0800

    linux-ib: Remove -headers and -docs from PKGBUILD

--- linux-ib/setup.sh
+++ linux-ib/setup.sh
@@ -1,8 +1,9 @@
 svn checkout svn://svn.archlinux.org/packages/linux/trunk linux-svn
 cp linux-svn/* .
-sed -i -e '/^pkgbase/d;'\
-          's/^#\(pkgbase=linux\)-custom/\1-infiniband/g;'\
-          's/^\(\s*pkgdesc.*modules\)"$/\1 with InfiniBand drivers"/g;' PKGBUILD
-
-sed -i -e '/^md5sums=(/,/)$/D' PKGBUILD
+sed -i -e '/^pkgbase/d;' \
+       -e 's/^#\(pkgbase=linux\)-custom/\1-infiniband/g;' \
+       -e 's/^\(\s*pkgdesc.*modules\)"$/\1 with InfiniBand drivers"/g;' \
+       -e '/^md5sums=(/,/)$/D' \
+       -e "/^makedepends=/s/\('xmlto'\|'docbook-xsl'\)\s*//g" \
+       -e 's/^pkgname=.*/pkgname=("${pkgbase}")/' PKGBUILD
 makepkg -g >> PKGBUILD
