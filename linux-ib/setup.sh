5b123d7124fcb888d0c0a8a501f33bd716fd9659
Anthony Cornehl <accornehl@gmail.com>
2013-11-06 08:25:52 -0800

    linux-ib: remove the docs and headers functions entirely

--- linux-ib/setup.sh
+++ linux-ib/setup.sh
@@ -1,9 +1,11 @@
 svn checkout svn://svn.archlinux.org/packages/linux/trunk linux-svn
 cp linux-svn/* .
-sed -i -e '/^pkgbase/d;' \
-       -e 's/^#\(pkgbase=linux\)-custom/\1-infiniband/g;' \
+sed -i -e '/^pkgbase/d;'                                                \
+       -e 's/^#\(pkgbase=linux\)-custom/\1-infiniband/g;'               \
        -e 's/^\(\s*pkgdesc.*modules\)"$/\1 with InfiniBand drivers"/g;' \
-       -e '/^md5sums=(/,/)$/D' \
-       -e "/^makedepends=/s/\('xmlto'\|'docbook-xsl'\)\s*//g" \
+       -e '/^md5sums=(/,/)$/D'                                          \
+       -e "/^makedepends=/s/\('xmlto'\|'docbook-xsl'\)\s*//g"           \
+       -e '/_package-\(docs\|headers\)() {/,/^}/D'                      \
+       -e 's|${pkgbase/linux/Linux}|Linux|p'                            \
        -e 's/^pkgname=.*/pkgname=("${pkgbase}")/' PKGBUILD
 makepkg -g >> PKGBUILD
